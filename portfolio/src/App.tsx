b8b4594f6fd55e842e5a69b4e7e7768ac3e9c305
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-11-16 17:10:03 -0300

    nova estrutura.
    adiocionado react router.
    global style ts adicionado.

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { Home } from "./pages/home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
---
+++ portfolio/src/global.ts
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #121212;
+    }
+
+    h1 {
+        color: var(--Primary-water-green-600, #00BA9B);
+        font-family: "Source Code Pro";
+        font-size: 51px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 48px; /* 94.118% */
+    }
+
+    h3 {
+        color: var(--Secondary-cyan-blue-400, #33B6D6);
+        font-family: "Source Code Pro";
+        font-size: 35px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 36px; /* 102.857% */
+    }
+
+    h4 {
+        color: var(--Secondary-cyan-blue-400, #33B6D6);
+        font-family: "Source Code Pro";
+        font-size: 29px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 36px; /* 124.138% */
+    }
+
+    h6 {
+        color: var(--Primary-water-green-900, #005647);
+        font-family: "Source Code Pro";
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 24px; /* 120% */
+    }
+
+    p {
+        color: var(--Neutral-gray-50, #F8F9F9);
+        text-align: justify;
+        font-family: "Fira Code";
+        font-size: 17px;
+        font-style: normal;
+        font-weight: 450;
+        line-height: 24px; /* 141.176% */
+    }
+
+`

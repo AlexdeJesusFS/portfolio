c7a3782710135d52d3002ec11c23b6e7949863db
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-11-19 23:35:10 -0300

    aplicado componente header na página home.

--- portfolio/src/pages/home/index.tsx
+++ portfolio/src/pages/home/index.tsx
@@ -1,16 +1,14 @@
-import { StyledWhatsappIcon, StyledCursorPointer, StyledCursorDeafult, StyledCursorText } from "./styles";
+import Header from "../../components/Header";
+
 
 function Home() {
   return (
     <>
+      <Header />
       <h3>Hello</h3>
       <h4>World</h4>
       <h6>Share</h6>
       <p>Paragraph...</p>
-      <StyledWhatsappIcon />
-      <StyledCursorDeafult />
-      <StyledCursorPointer />
-      <StyledCursorText />
     </>
   );
 }

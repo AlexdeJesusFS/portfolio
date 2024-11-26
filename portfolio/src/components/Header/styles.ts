8c450c63b0eabe93de1b2bc9ae5f320ba7a06515
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-11-26 19:23:14 -0300

    Ajuste de altura do header.
    Header menor com 60px agora, melhor assim para não afetar a leitura.

--- portfolio/src/components/Header/styles.ts
+++ portfolio/src/components/Header/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.header`
     display: flex;
     width: 100%;
-    height: 80px;
+    height: 60px;
     padding: 16px 144px;
     justify-content: flex-end;
     align-items: flex-start;
@@ -15,13 +15,14 @@ export const Container = styled.header`
     background: var(--Primary-water-green-50, #E6FAF7);
 `
 
-export const LeftSide = styled.div`
-    display: flex;
-    height: 48px;
-    align-items: center;
-    gap: 10px;
-    flex: 1 0 0;
-`
+    export const LeftSide = styled.div`
+        display: flex;
+        height: 100%;
+        padding: 15px 0px;
+        align-items: center;
+        gap: 10px;
+        flex: 1 0 0;
+    `
 
 export const Menu = styled.div`
     display: flex;

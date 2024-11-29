5623f8f5441ceae03b5ec9767d379df04c55e96d
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-11-29 20:25:26 -0300

    ajuste no padding, alinhado ao conteúdo.

--- portfolio/src/components/Header/styles.ts
+++ portfolio/src/components/Header/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.header`
     display: flex;
     width: 100%;
     height: 60px;
-    padding: 16px 144px;
+    padding: 16px 96px;
     justify-content: flex-end;
     align-items: flex-start;
     gap: 10px;

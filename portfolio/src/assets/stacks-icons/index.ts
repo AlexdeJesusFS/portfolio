14309f3b9159dbb351c30de2d00e285b462b7f99
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-11-29 22:51:26 -0300

    Novas imagens.png adicionadas.
    Pasta stacks-icons com imagens para as stacks que fazem parte do carrossel (carousel)

---
+++ portfolio/src/assets/stacks-icons/index.ts
@@ -0,0 +1,32 @@
+import figmaIcon from "./figma.png"; 
+import htmlIcon from "./html5.png"; 
+import cssIcon from "./css3.png"; 
+import jsIcon from "./js.png"; 
+import tsIcon from "./ts.png"; 
+import reactIcon from "./react.png"; 
+import pythonIcon from "./python.png";
+import csharpIcon from "./csharp.png";
+import dotnetIcon from "./dotnet.png";
+import sqlserverIcon from "./sqlserver.png";
+import mysqlIcon from "./mysql.png";
+import gitIcon from "./git.png";
+import dockerIcon from "./docker.png";
+
+
+const icons = {
+    figmaIcon,
+    htmlIcon,
+    cssIcon,
+    jsIcon,
+    tsIcon,
+    reactIcon,
+    pythonIcon,
+    csharpIcon,
+    dotnetIcon,
+    sqlserverIcon,
+    mysqlIcon,
+    gitIcon,
+    dockerIcon,
+};
+
+export default icons;

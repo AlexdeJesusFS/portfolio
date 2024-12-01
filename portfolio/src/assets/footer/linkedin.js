9bf565d39c24954cc428450a28a421d29c012ae6
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-01 16:30:05 -0300

    Reorganizando.
    renomeada pasta contacts-icons para footer
    novo computer gif sem fundo

--- portfolio/src/assets/footer/linkedin.js
+++ portfolio/src/assets/footer/linkedin.js
@@ -1,4 +1,4 @@
-const linkedin = ({color="#053032", size="40px", ...props}) => (
+const linkedin = ({color="#053032", size="40px"}) => (
 
     <svg width={size} height={size} 
     viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">

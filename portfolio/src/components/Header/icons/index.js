f386556804ae7c4abfbaf62ee855e4adb05f3b79
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-02 23:39:29 -0300

    Pasta com compoentes para icons de header.
    componentes js de icons usados em header adiconados a pasta do componente.

---
+++ portfolio/src/components/Header/icons/index.js
@@ -0,0 +1,4 @@
+export { default as AboutIcon } from './about-circle';
+export { default as CertificateIcon } from './certificate';
+export { default as ChatIcon } from './chat';
+export { default as ConstructIcon } from './construct';

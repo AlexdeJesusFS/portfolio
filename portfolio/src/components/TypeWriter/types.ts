8c290081f69b0a08274913ecd304982153f1db76
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-07 01:09:59 -0300

    Novo componente TypeWriter.
    criado para animação de auto typing, animação de digitação;
    com paramêtros para alterar frases e tempo.

---
+++ portfolio/src/components/TypeWriter/types.ts
@@ -0,0 +1,9 @@
+
+export interface ITypewriterProps {
+    phrases: string[]; // Lista de frases
+    typeSpeed: number; // Velocidade da digitação (ms)
+    backSpeed: number; // Velocidade para apagar (ms)
+    loop: boolean; // Se a animação repete
+    backDelay: number;
+    startDelay: number;
+};

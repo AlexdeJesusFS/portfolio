ce7a03ea7e703e4590a3225a2cea6a0f3a9acadd
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-09 22:36:13 -0300

    Header atualizado.
    removido botão para seção certificações, currículo e portfolio não são a mesma coisa;
    nova função para onClick de botão "Alex de Jesus" com redirecionamento para raíz e som de click.

--- portfolio/src/components/Header/index.tsx
+++ portfolio/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Container, LeftSide, Menu, IconContainer, HeaderTitle } from "./styles"
 import HeaderButton from "../HeaderButton/index";
 import clickButtonSound from "../../assets/sounds/buttonClicking.wav"
 
-import { AboutIcon, CertificateIcon, ChatIcon, ConstructIcon } from './icons';
+import { AboutIcon, ChatIcon, ConstructIcon } from './icons';
 
 import {useNavigate} from "react-router-dom";
 
@@ -15,16 +15,21 @@ function Header() {
         const audio = new Audio(clickButtonSound);
         audio.play();
     };
+    
+    function clickTitle() {
+        const audio = new Audio(clickButtonSound);
+        audio.play();
+        navigate("/");
+    }
 
     return (
         <Container>
             <LeftSide>
-                <HeaderTitle onClick={() => navigate("/")} >Alex de Jesus</HeaderTitle>
+                <HeaderTitle onClick={clickTitle} >Alex de Jesus</HeaderTitle>
             </LeftSide>
             <Menu>
                 <HeaderButton href="#sobre" onClick={clickSound} >Sobre{<IconContainer><AboutIcon /></IconContainer>}</HeaderButton>
                 <HeaderButton href="#projetos" onClick={clickSound} >Projetos{<IconContainer><ConstructIcon /></IconContainer>}</HeaderButton>
-                <HeaderButton href="#certificações" onClick={clickSound} >Certificações{<IconContainer><CertificateIcon /></IconContainer>}</HeaderButton>
                 <HeaderButton href="#fale_comigo" onClick={clickSound} >Fale comigo{<IconContainer><ChatIcon /></IconContainer>}</HeaderButton>
             </Menu>
         </Container>

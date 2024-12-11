9b59b4967407bf706981c6476cc2e960dae98310
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-10 23:44:54 -0300

    Página Home atualizada.
    novos componentes reorganizados adicionados e página restruturada e refatorada.

--- portfolio/src/pages/home/index.tsx
+++ portfolio/src/pages/home/index.tsx
@@ -1,21 +1,16 @@
 import Header from "../../components/Header";
-import { 
-  AnimationsContainer, SectionAbout, SectionProjects, ContainerCards, ColumnCards, SectionStacks, Container
-} from "./styles";
-import Card from "../../components/Card";
-import CardTag from "../../components/CardTag";
-import { htmlIcon, cssIcon, tsIcon, figmaIcon, reactIcon, pythonIcon, apiIcon, sqlserverIcon } from "../../assets/dev-icon-tags/index"; 
-import "../../assets/dev-icon-tags/";
-import Carousel from "../../components/Carousel";
+import { AnimationsContainer, Container } from "./styles";
+import EllipsesAnimation from "./components/EllipsesAnimation";
+import TypeWrite from "./components/TypeWriter";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import StacksSection from "./components/StacksSection";
 import Footer from "../../components/Footer";
-import EllipesesBackground from "../../components/EllipsesBackground";
-import TypeWrite from "../../components/TypeWriter";
-import EllipsesAnimation from "../../components/EllipsesAnimation";
-import DownloadCVButton from "../../components/DownloadButton"
 
+
+const listPhrases = ["Front-end", "Back-end", "Full-Stack"]
 
 function Home() {
-  const listPhrases = ["Front-end", "Back-end", "Full-Stack"]
 
   return (
     <>
@@ -29,63 +24,11 @@ function Home() {
           loop={false} backDelay={1000} startDelay={4500}/>
         </AnimationsContainer>
 
-        <p id="sobre"></p>
-        <br />
-        <SectionAbout>        
-          <h3>Sobre mim.</h3>
-          <p>Sou um profissional com <strong>mais de 2 anos de experiência em desenvolvimento </strong>
-            realizando <strong>integrações</strong> e <strong>automações</strong> em <strong>Python</strong>, focado em otimizar processos 
-            internos e resolver desafios técnicos. Meu trabalho inclui o gerenciamento de 
-            chamados técnicos e o cumprimento de SLAs. Estou familiarizado com <strong>metodologias 
-            ágeis</strong>, como <strong>Scrum</strong> e <strong>Kanban</strong>, e práticas do <strong>PMBOK para gerenciar projetos</strong>, já 
-            trabalhei em equipe cumprindo metas por meio do uso de OKRs e feedbacks 
-            contínuos. Sempre buscando melhorar o desempenho individual e da equipe.</p>
-          <DownloadCVButton />
-        </SectionAbout>
-        
-        <SectionProjects>
-        <EllipesesBackground className="ellipses-background"/>
-          <p id="projetos"></p>
-          <br />
-          <h3>Projetos.</h3>
-          <ContainerCards>
-            <ColumnCards>{/*Left*/}
-              <Card title="ELGScreen." text="Automação para atualizar o estoque do ERP, onde arquivos XML eram capturados via API, os erros nos arquivos eram corrigidos e, por fim, os dados eram enviados para o servidor do ERP. Projeto desenvolvido utilizando Python.">
-                <CardTag text="API"><img src={apiIcon} alt="API ícone" /></CardTag>
-                <CardTag text="Python"><img src={pythonIcon} alt="Python ícone" /></CardTag>
-              </Card>
-              <Card title="ELGScreen." text="Automação desenvolvida em Python para atualizar o status dos pedidos entregues no Mercado Livre, agilizando o processo de liberação de faturamento da empresa. A solução utiliza a API dos Correios para verificar o status de entrega dos pedidos e a API do Mercado Livre para atualizar os respectivos status.">
-                <CardTag text="API"><img src={apiIcon} alt="API ícone" /></CardTag>
-                <CardTag text="Python"><img src={pythonIcon} alt="Python ícone" /></CardTag>
-              </Card>
-              <Card title="Projeto pessoal." text="Este site também conta, certo? Neste projeto pratiquei mais habilidades front-end, usando HTML, CSS, TypeScript e o framework React. O protótipo do projeto foi feito no Figma e com base nele criado o código.">
-                <CardTag text="HTML"><img src={htmlIcon} alt="HTML ícone" /></CardTag>
-                <CardTag text="CSS"><img src={cssIcon} alt="CSS ícone" /></CardTag>
-                <CardTag text="TypeScript"><img src={tsIcon} alt="TypeScript ícone" /></CardTag>
-                <CardTag text="React"><img src={reactIcon} alt="React ícone" /></CardTag>
-                <CardTag text="Figma"><img src={figmaIcon} alt="Figma ícone" /></CardTag>
-              </Card>
-            </ColumnCards>
-            <ColumnCards>{/*Right*/}
-              <Card title="G2." text="Desenvolvimento de uma integração entre o Service Desk Movidesk e o WhatsApp para envio de mensagens aos clientes, utilizando Python e a API do 360Dialog para a comunicação com o WhatsApp. A solução atualizava os clientes sobre seus tickets sempre que uma alteração de status era realizada. Para isso, foi criada uma API com FastAPI para receber os webhooks do Movidesk e enviar as mensagens automaticamente.">
-                <CardTag text="API"><img src={apiIcon} alt="API ícone" /></CardTag>
-                <CardTag text="Python"><img src={pythonIcon} alt="Python ícone" /></CardTag>
-              </Card>
-              <Card title="ELGScreen." text="Desenvolvimento de uma ferramenta para listar os produtos mais vendidos no Mercado Livre que a empresa não comercializa. Utilizando a API do Mercado Livre, foi gerada uma lista dos produtos mais vendidos em uma determinada categoria. Em seguida, foi realizada uma consulta ao banco de dados SQL Server da empresa para obter a lista de produtos vendidos na mesma categoria. Com o uso de expressões regulares e um framework de correspondência de texto, foi possível identificar correspondências superiores a 70% nos títulos. Os produtos mais vendidos do Mercado Livre que não estavam no portfólio da empresa foram exibidos em uma tabela na intranet, com imagens e links. A solução foi implementada com HTML, CSS e Python Django.">
-                <CardTag text="API"><img src={apiIcon} alt="API ícone" /></CardTag>
-                <CardTag text="Python"><img src={pythonIcon} alt="Python ícone" /></CardTag>
-                <CardTag text="SQL Server"><img src={sqlserverIcon} alt="SQL Server ícone" /></CardTag>
-                <CardTag text="HTML"><img src={htmlIcon} alt="HTML ícone" /></CardTag>
-                <CardTag text="CSS"><img src={cssIcon} alt="CSS ícone" /></CardTag>
-              </Card>
-            </ColumnCards>
-          </ContainerCards>
-        </SectionProjects>
+        <AboutSection />
 
-        <SectionStacks>
-          <h4>Tecnologias.</h4>
-          <Carousel />
-        </SectionStacks>
+        <ProjectsSection />
+        
+        <StacksSection />
 
       </Container>
 
--- portfolio/src/pages/home/styles.ts
+++ portfolio/src/pages/home/styles.ts
@@ -12,7 +12,7 @@ export const Container = styled.div`
     gap: 44px;
 
     .ellipses-background {
-        top: 1380px;
+        top: 1550px;
     }
 `
 
@@ -23,81 +23,4 @@ export const AnimationsContainer = styled.div`
     justify-content: center; /* Alinha horizontalmente */
     height: 532px;
     gap: 62px;
-`
-
-export const SectionAbout = styled.section`
-    display: flex;
-    width: 100%;
-    //height: 292px;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    flex-shrink: 0;
-    //margin: auto;
-    gap: 44px;
-`
-
-export const SectionProjects = styled.section`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 252px auto;
-    gap: 44px;
-`
-
-export const ContainerCards = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 24px;
-    align-self: stretch;
-`
-
-export const ColumnCards = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-    align-items: center;
-    align-self: stretch;
-    gap: 24px;
-`
-
-export const SectionCertifications = styled.section`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    align-items: flex-start;
-    //margin: auto;
-    gap: 44px;
-`
-
-export const ContainerCertifications = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 35px;
-    align-self: stretch;
-`
-
-export const Certification = styled.div`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 10px;
-`
-
-export const SectionStacks = styled.section`
-    display: flex;
-    width: 98.7vw;//largura correta para evitar scroll horizontal
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 84px;
-    margin-top: 84px;
-    margin-bottom: 154px;
-
-    h4 {
-        margin-left: 142px;
-    }
 `

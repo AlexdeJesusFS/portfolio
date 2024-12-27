ce57e64b64cf99ff247713a8c4884a84cf222805
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-12-26 23:58:14 -0300

    Componente seção about refatorado.
    alterado código da tag section
    uso de unidade rem para responsividade

--- portfolio/src/pages/home/components/AboutSection/styles.ts
+++ portfolio/src/pages/home/components/AboutSection/styles.ts
@@ -3,10 +3,8 @@ import styled from "styled-components";
 
 export const Section = styled.section`
     display: flex;
-    width: 100%;
     flex-direction: column;
-    justify-content: space-between;
     align-items: flex-start;
-    flex-shrink: 0;
-    gap: 44px;
+    gap: 2.75rem;
+    align-self: stretch;
 `

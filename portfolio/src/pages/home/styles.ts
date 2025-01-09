351dc324d8beff46d2b3513d3e279cb22a7346ad
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-08 21:06:22 -0300

    AnimationsContainer atualizado.
    width é igual a 100% agora, dessa forma não está cortando o tamanho do conteúdo interno.

--- portfolio/src/pages/home/styles.ts
+++ portfolio/src/pages/home/styles.ts
@@ -3,17 +3,17 @@ import styled from "styled-components";
 
 export const MainContainer = styled.main`
     display: flex;
-    padding: 12.8rem 14.4rem;
+    padding: 12.8rem 14.4rem; //TODO: clamp
     flex-direction: column;
     align-items: center;
-    gap: 20.6rem;
+    gap: 20.6rem; //TODO: clamp
     align-self: stretch;
 `
 
 export const AnimationsContainer = styled.div`
     display: flex;
-    width: 70.4rem;
+    width: 100%;
     flex-direction: column;
     align-items: center;
-    gap: 4.2rem;
+    gap: 8.2rem; //TODO: clamp
 `

f44dc185989831e54ea30f46f95f2f123ba7ad3b
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-10 23:14:52 -0300

    page home com responsividade.
    adicionado responsividade com medias queries.

--- portfolio/src/pages/home/styles.ts
+++ portfolio/src/pages/home/styles.ts
@@ -1,13 +1,24 @@
 import styled from "styled-components";
+import media from "../../media";
 
 
 export const MainContainer = styled.main`
     display: flex;
-    padding: 12.8rem 14.4rem; //TODO: clamp
+    padding: 12.8rem 14.4rem;
     flex-direction: column;
     align-items: center;
-    gap: 20.6rem; //TODO: clamp
+    gap: 20.6rem;
     align-self: stretch;
+
+    ${media.tablet(`
+        padding: 8.6rem 3.2rem;
+        gap: 17.4rem;
+    `)}
+
+    ${media.mobile(`
+        padding: 3.8rem 1.6rem;
+        gap: 8.7rem;
+    `)}
 `
 
 export const AnimationsContainer = styled.div`
@@ -15,5 +26,9 @@ export const AnimationsContainer = styled.div`
     width: 100%;
     flex-direction: column;
     align-items: center;
-    gap: 8.2rem; //TODO: clamp
+    gap: 8.2rem;
+
+    ${media.mobile(`
+        gap: 2.1rem;
+    `)}
 `

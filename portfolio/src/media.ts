6b2a9d57d893ac551d8735039a7976e0ac6778e6
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-10 23:13:30 -0300

    media.ts atualizado com novos valores para medias queries

--- portfolio/src/media.ts
+++ portfolio/src/media.ts
@@ -2,9 +2,14 @@ import theme from "./theme/theme"
 
 
 const media = {
-    mobile: (styles: string) => `@media (max-width: ${theme.breakpoints.mobile}) {${styles}}`,
-    tablet: (styles: string) => `@media (max-width: ${theme.breakpoints.tablet}) {${styles}}`,
-    desktop: (styles: string) => `@media (min-width: ${theme.breakpoints.desktop}) {${styles}}`,
-  };
+  mobile: (styles: string) =>
+    `@media (max-width: ${theme.breakpoints.mobile}) {${styles}}`,
+    
+  tablet: (styles: string) =>
+    `@media (min-width: ${theme.breakpoints.tablet}) and (max-width: ${theme.breakpoints.desktop}) {${styles}}`,
+    
+  desktop: (styles: string) =>
+    `@media (min-width: ${theme.breakpoints.desktop}) {${styles}}`,
+};
 
 export default media;

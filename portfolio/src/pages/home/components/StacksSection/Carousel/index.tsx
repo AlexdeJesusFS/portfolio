4c6ee6f6b3615df7a4bdb7447da7f179e5ac6366
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-20 23:43:30 -0300

    código refatorado.
    valor de icon usado como key na tag <li> em função map
    valor único para chave diferente de index para  não usar pode um valor que pode ser alterado dinamicamente

--- portfolio/src/pages/home/components/StacksSection/Carousel/index.tsx
+++ portfolio/src/pages/home/components/StacksSection/Carousel/index.tsx
@@ -1,24 +1,24 @@
+import icons from "../../../../../assets/stacks-icons";
 import { Container, DevIcons } from "./styles";
-import icons from "../../../../../assets/stacks-icons"
-
 
 function Carousel() {
-    //obtendo os valores de `icons` e duplicando.
-    const iconList = [...Object.values(icons), ...Object.values(icons)];
+	//obtendo os valores de `icons` e duplicando.
+	const iconList = [...Object.values(icons), ...Object.values(icons)];
 
-    return (
-        <Container>
-            <DevIcons>
-                <ul>
-                    {iconList.map((icon, index) => (
-                        <li key={index}>
-                            <img src={icon} alt={`Icone ${index}`} />
-                        </li>
-                    ))}
-                </ul>
-            </DevIcons>
-        </Container>
-    );
+	return (
+		<Container>
+			<DevIcons>
+				<ul>
+					{iconList.map((icon, index) => (
+						//uso de um valor único diferente de index para um valor que não pode ser alterado dinamicamente
+						<li key={icon}>
+							<img src={icon} alt={`Icone ${index}`} />
+						</li>
+					))}
+				</ul>
+			</DevIcons>
+		</Container>
+	);
 }
 
 export default Carousel;

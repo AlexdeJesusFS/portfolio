af62ed9e83ced309b9781699fa83319e83ba12f4
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-22 18:07:08 -0300

    Assets atualizados.
    gif de computador substituido por formato em video, nova pasta "videos" com videos em webp e mp4;
    imagens da pasta "stacks-icons" substituidas por formato webp, compressão melhor, melhorando o desempenho do site;

--- portfolio/src/assets/stacks-icons/index.ts
+++ portfolio/src/assets/stacks-icons/index.ts
@@ -1,31 +1,31 @@
-import csharpIcon from "./csharp.png";
-import cssIcon from "./css3.png";
-import dockerIcon from "./docker.png";
-import dotnetIcon from "./dotnet.png";
-import figmaIcon from "./figma.png";
-import gitIcon from "./git.png";
-import htmlIcon from "./html5.png";
-import jsIcon from "./js.png";
-import mysqlIcon from "./mysql.png";
-import pythonIcon from "./python.png";
-import reactIcon from "./react.png";
-import sqlserverIcon from "./sqlserver.png";
-import tsIcon from "./ts.png";
+import csharpIcon from "./csharp.webp";
+import cssIcon from "./css3.webp";
+import dockerIcon from "./docker.webp";
+import dotnetIcon from "./dotnet.webp";
+import figmaIcon from "./figma.webp";
+import gitIcon from "./git.webp";
+import htmlIcon from "./html5.webp";
+import jsIcon from "./js.webp";
+import mysqlIcon from "./mysql.webp";
+import pythonIcon from "./python.webp";
+import reactIcon from "./react.webp";
+import sqlserverIcon from "./sqlserver.webp";
+import tsIcon from "./ts.webp";
 
-const icons = {
-	figmaIcon,
-	htmlIcon,
-	cssIcon,
-	jsIcon,
-	tsIcon,
-	reactIcon,
-	pythonIcon,
-	csharpIcon,
-	dotnetIcon,
-	sqlserverIcon,
-	mysqlIcon,
-	gitIcon,
-	dockerIcon,
-};
+const icons = [
+	{ src: figmaIcon, name: "Figma" },
+	{ src: htmlIcon, name: "HTML5" },
+	{ src: cssIcon, name: "CSS3" },
+	{ src: jsIcon, name: "JavaScript" },
+	{ src: tsIcon, name: "TypeScript" },
+	{ src: reactIcon, name: "React" },
+	{ src: pythonIcon, name: "Python" },
+	{ src: csharpIcon, name: "C#" },
+	{ src: dotnetIcon, name: ".NET" },
+	{ src: sqlserverIcon, name: "SQL Server" },
+	{ src: mysqlIcon, name: "MySQL" },
+	{ src: gitIcon, name: "Git" },
+	{ src: dockerIcon, name: "Docker" },
+];
 
 export default icons;

b9b223626ac7272923d0a401bb51bcf2fc5cfcba
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-01-22 18:12:21 -0300

    adicionado suporte a arquivos webm e mp4

--- portfolio/src/custom.d.ts
+++ portfolio/src/custom.d.ts
@@ -12,3 +12,13 @@ declare module "*.pdf" {
 	const src: string;
 	export default src;
 }
+
+declare module "*.webm" {
+	const src: string;
+	export default src;
+}
+
+declare module "*.mp4" {
+	const src: string;
+	export default src;
+}

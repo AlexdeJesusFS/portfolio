2fa32c74256972b4e37c6c4c3f001bafb3ad81a3
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-03-16 14:19:45 -0300

    removido código para desempenho da biblioteca lottie, pois não é mais usada

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    // Configurações para SCSS
-    preprocessorOptions: {
-      scss: {
-        // Adicione opções específicas do SCSS aqui, se necessário
-        // Exemplo: additionalData: `@import "src/styles/variables.scss";`
-      },
-    },
-  },
-  build: {
-    // Configurações de build para otimizar code splitting
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"], // Separa bibliotecas grandes em chunks distintos
-          lottie: ["lottie-web"], // Separa lottie-web para reduzir o chunk principal
-        },
-      },
-    },
-    // Minificação de CSS é habilitada por padrão em produção com 'esbuild'
-    minify: "esbuild", // Garante que o JavaScript e CSS sejam minificados
-  },
 });

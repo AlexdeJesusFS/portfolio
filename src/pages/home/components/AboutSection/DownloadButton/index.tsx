bffeda03514e8d779657d028fd165ec6ab8dae11
AlexdeJesusFS <alexjesus0116@gmail.com>
2025-03-17 14:03:11 -0300

    Alterado caminho de `Curriculo.pdf` para `public/`, para que alterações no arquivo não afetem o build, dessa forma alterações no arquivo não precisam de um re-build.

--- src/pages/home/components/AboutSection/DownloadButton/index.tsx
+++ src/pages/home/components/AboutSection/DownloadButton/index.tsx
@@ -1,10 +1,10 @@
-import Cv from "../../../../../assets/download-cv-button/Curriculo.pdf";
+import cv from "/Curriculo.pdf";
 import DownloadIcon from "../../../../../assets/download-cv-button/file-download.svg";
 import styles from "./styles.module.css";
 
 function DownloadButton() {
   return (
-    <a href={Cv} style={{ textDecoration: "none", color: "inherit" }}>
+    <a href={cv} style={{ textDecoration: "none", color: "inherit" }}>
       <button className={styles.button} type="button">
         {"Baixar CV"} <img src={DownloadIcon} alt="arquivo download ícone" />
       </button>
